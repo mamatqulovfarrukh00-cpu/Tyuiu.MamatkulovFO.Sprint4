0d8d60a7dbbc9d704a9b62130b3f447b6daa9cfb
Фаррух Маматкулов <farrukh0308@inbox.ru>
2025-11-02 13:02:56 +0500

    Task1

--- Tyuiu.MamatkulovFO.Sprint4.Task1.V22.Test/DataServiceTest.cs
+++ Tyuiu.MamatkulovFO.Sprint4.Task1.V22.Test/DataServiceTest.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Tyuiu.MamatkulovFO.Sprint4.Task1.V22.Lib;
 namespace Tyuiu.MamatkulovFO.Sprint4.Task1.V22.Test
-{
-
-    
-
+{  
     [TestFixture]
     public class DataServiceTest
     {
@@ -16,12 +13,6 @@ namespace Tyuiu.MamatkulovFO.Sprint4.Task1.V22.Test
             Assert.AreEqual(295245, service.CalculateOddProduct(array));
         }
 
-        [Test]
-        public void CalculateOddProduct_UniqueOnly_Returns135()
-        {
-            var service = new DataService();
-            int[] array = { 8, 5, 4, 4, 3, 9, 9, 9, 3, 4, 4, 9 };
-            Assert.AreEqual(135, service.CalculateOddProduct(array, true));
-        }
+       
     }
 }
